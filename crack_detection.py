3edc417b8598c95244b5aa43996b615f1c03e5b7
Reemas <reemasqanadilo2006@gmail.com>
2026-07-22 23:33:31 +0300

    big edit

--- crack_detection.py
+++ crack_detection.py
@@ -1,4 +1,5 @@
 import os
+from torchvision import transforms
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'TRUE'
 import torch          
 import torchvision    
@@ -25,27 +26,42 @@ print(f"Negative: {len(image_negative)}")
 print(f"Positive: {len(image_positive)}")
 #make a tensor
 to_tensor = torchvision.transforms.ToTensor()
+transform_train = transforms.Compose([
+    transforms.Resize((227, 227)),
+    transforms.RandomHorizontalFlip(p=0.5),
+    transforms.RandomVerticalFlip(p=0.5),
+    transforms.RandomRotation(degrees=15),
+    transforms.ColorJitter(brightness=0.3, contrast=0.3),
+    transforms.ToTensor()
+])
+
+transform_test = transforms.Compose([
+    transforms.Resize((227, 227)),
+    transforms.ToTensor()
+])
 #function : conver the image to tensor 
-def image_to_tensor(images,label):
-    image_tensor=[]
+def image_to_tensor(images, label, transform):
+    image_tensor = []
     for image in images:
-       image=Image.open(image)
-       image= to_tensor(image)
-       image_tensor.append((image,label))
+        img = Image.open(image).convert("RGB")
+        img = transform(img)
+        image_tensor.append((img, label))
     return image_tensor
 #The Dataset after conver 
-negative_data = image_to_tensor(image_negative, 0)
-positive_data = image_to_tensor(image_positive, 1)
-dataset = negative_data + positive_data
-random.shuffle(dataset)
-print(f"Total: {len(dataset)}")
+negative_data = image_to_tensor(image_negative, 0, transform_train)
+positive_data = image_to_tensor(image_positive, 1, transform_train)
+negative_test = image_to_tensor(image_negative, 0, transform_test)
+positive_test = image_to_tensor(image_positive, 1, transform_test)
+dataset_train = negative_data + positive_data
+dataset_test = negative_test + positive_test
+random.shuffle(dataset_train)
+random.shuffle(dataset_test)
+
+train_data = dataset_train
+test_data = dataset_test
+print(f"Total: {len(dataset_train) + len(dataset_test)}")
 
 #make training and testing data
-
-split=int(0.8*(len(dataset)))
-train_data=dataset[:split]
-test_data=dataset[split:]
-print(f"split{split}    train{len(train_data)}  test{len(test_data)}")
 
 data_train_loader=DataLoader(train_data,batch_size=32,shuffle=True)
 data_test_loader=DataLoader(test_data,batch_size=32,shuffle=False)
@@ -110,7 +126,6 @@ print(f"Accuracy: {accuracy:.2f}%")
 torch.save(model.state_dict(), 'crack_model.pth')
 print("Model saved!")
 
-from torchvision import transforms
 
 transform = transforms.Compose([
     transforms.Resize((227, 227)),
