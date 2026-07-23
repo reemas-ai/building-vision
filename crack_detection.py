cd2c9b2ce86e475685dd038ad6bc1b21498fa260
Reemas <reemasqanadilo2006@gmail.com>
2026-07-23 12:43:20 +0300

    Norm

--- crack_detection.py
+++ crack_detection.py
@@ -34,7 +34,7 @@ transform_train = transforms.Compose([
     transforms.RandomVerticalFlip(p=0.5),
     transforms.RandomRotation(degrees=15),
     transforms.ColorJitter(brightness=0.3, contrast=0.3),
-    transforms.ToTensor()
+    transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], 
                      std=[0.229, 0.224, 0.225])
 ])
